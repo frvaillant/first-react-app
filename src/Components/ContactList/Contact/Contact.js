4f5bf06454240e805917346b4c63315f6cea3f43
francoisvaillant <frvaillant@gmail.com>
2020-07-05 19:08:25 +0200

    transform contact in class add states and handleclick on status

--- src/Components/ContactList/Contact/Contact.js
+++ src/Components/ContactList/Contact/Contact.js
@@ -3,23 +3,35 @@ import React from "react";
 
 import './contact.css';
 
-const Contact = (props) => {
-    return (
-        <div className="Contact">
-            <img src={props.avatar}  className="avatar" alt={props.alt} />
-            <div className="status">
-                <h4 className="name">{props.name}</h4>
-                <div className={props.online ? 'status-online' : 'status-offline'} />
-                <div className="status-text">{props.online ? 'Online' : 'Offline'}</div>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ online: !this.state.online });
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} className="avatar" alt={this.props.alt}/>
+                <div className="status"  onClick={this.handleClick}>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className={this.state.online ? 'status-online' : 'status-offline'}/>
+                    <div className="status-text">{this.state.online ? 'Online' : 'Offline'}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    online: PropTypes.bool
 };
 
 export default Contact
